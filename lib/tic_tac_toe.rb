dc9e67d6fe72a1e2ab55b3942f6b4c015ab8896b
Carrie Savinell <carrielane89@gmail.com>
2018-04-06 21:33:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,8 +36,8 @@ class TicTacToe
     @board[location] == "X" || @board[location] == "O"
    end      
  
-  def valid_move?(location)
-  index.between?(0,8) && !position_taken?(location)
+  def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
   end
 
 end   
